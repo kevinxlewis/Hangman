7de8194d13a09893ca879ba40b35aeb196d1194f
Kevin Lewis <kevinjlewis0@gmail.com>
2022-08-15 17:00:29 -0230

    -> Cleaned up code and changed approach to some methods for simplicity. -> Re-worked drawing the hanged man for readability and to have a more traditional look in the console. -> Added comments. -> Removed audio code due to persisting errors. -> Final commit for submission.

--- src/GameDisplayAssets.java
+++ src/GameDisplayAssets.java
@@ -2,30 +2,61 @@ public class GameDisplayAssets {
     public static void displayHangingMan(Integer numberOfWrongGuesses) {
         // Displaying the post that the man will be hanging from related
         // to the amount of times a player has an incorrect guess.
-        System.out.println(" _________ ");
-        System.out.println(" |       |");
-
-        if(numberOfWrongGuesses >= 1) {
-            System.out.println(" O");
+        if(numberOfWrongGuesses == 0) {
+            System.out.println(" ------------  ");
+            System.out.println(" |          |  ");
+            System.out.println(" |             ");
+            System.out.println(" |             ");
+            System.out.println(" |             ");
+            System.out.println(" |             ");
         }
-        if(numberOfWrongGuesses >= 2) {
-            System.out.print("\\ ");
-            if(numberOfWrongGuesses >=3) {
-                System.out.println("/");
-            } else {
-                System.out.println("");
-            }
+        if (numberOfWrongGuesses == 1) {
+            System.out.println(" ------------   ");
+            System.out.println(" |          |   ");
+            System.out.println(" |          0   ");
+            System.out.println(" |              ");
+            System.out.println(" |              ");
+            System.out.println(" |              ");
         }
-        if(numberOfWrongGuesses >= 4) {
-            System.out.println(" |");
+        if (numberOfWrongGuesses == 2) {
+            System.out.println(" ------------    ");
+            System.out.println(" |          |    ");
+            System.out.println(" |          0    ");
+            System.out.println(" |        \\     ");
+            System.out.println(" |               ");
+            System.out.println(" |               ");
         }
-        if(numberOfWrongGuesses >= 5) {
-            System.out.print("/ ");
-            if(numberOfWrongGuesses >= 6) {
-                System.out.println("\\");
-            } else {
-                System.out.println("");
-            }
+        if (numberOfWrongGuesses == 3) {
+            System.out.println(" ------------    ");
+            System.out.println(" |          |    ");
+            System.out.println(" |          0    ");
+            System.out.println(" |         \\|   ");
+            System.out.println(" |               ");
+            System.out.println(" |               ");
+        }
+        if (numberOfWrongGuesses == 4) {
+            System.out.println(" ------------    ");
+            System.out.println(" |          |    ");
+            System.out.println(" |          0    ");
+            System.out.println(" |         \\|/  ");
+            System.out.println(" |               ");
+            System.out.println(" |               ");
+        }
+        if (numberOfWrongGuesses == 5) {
+            System.out.println(" ------------    ");
+            System.out.println(" |          |    ");
+            System.out.println(" |          0    ");
+            System.out.println(" |         \\|/  ");
+            System.out.println(" |         /     ");
+            System.out.println(" |               ");
+        }
+        if (numberOfWrongGuesses == 6) {
+            System.out.println(" ------------    ");
+            System.out.println(" |          |    ");
+            System.out.println(" |          0    ");
+            System.out.println(" |         \\|/  ");
+            System.out.println(" |         / \\  ");
+            System.out.println(" |               ");
         }
         System.out.println();
         System.out.println();
--- src/Hangman.java
+++ src/Hangman.java
@@ -6,10 +6,13 @@ import java.io.*;
 
 
 public class Hangman {
-
+    static int numberOfWrongGuesses = 0;
     public static void main(String[] args) {
         Scanner readerObj = null;
         List<String> words = new ArrayList<>();
+
+        // Reading word bank file and adding the words to
+        // an array list
         try {
             readerObj = new Scanner(new File("src/wordbank.txt"));
             while (readerObj.hasNextLine()) {
@@ -17,61 +20,84 @@ public class Hangman {
             }
         }
         catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         finally {
             assert readerObj != null;
             readerObj.close();
         }
 
+
         Random rand = new Random();
         String word = words.get(rand.nextInt(words.size()));
         List<Character> guesses = new ArrayList<>();
-        int numberOfWrongGuesses = 0;
         Scanner input = new Scanner(System.in);
-        AudioAssets audioAssets = new AudioAssets();
 
         System.out.println("Welcome to Hangman!");
+
         while(true) {
 
             GameDisplayAssets.displayHangingMan(numberOfWrongGuesses);
+            displayWordProgress(word, guesses);
+
+            // Keeping track of the guesses the player makes so that they are
+            // sure not to duplicate
+            System.out.println("\nLetters Guessed: " + guesses);
+
+            // Checking to see if limit of incorrect guesses has been met
+            // If true - game is over and player has lost
             if(numberOfWrongGuesses >= 6) {
-                System.out.println("Game over!\tThe correct word was: " + word);
+                System.out.println("Game over! :(  The correct word was: " + word);
                 break;
             }
-            displayWordProgress(word, guesses);
-            if(!getGuessesOfPlayer(input, word, guesses)) {
-                numberOfWrongGuesses++;
-                System.out.println("Sorry! That guess wasn't right. Try again.\n");
-                System.out.println("Wrong guesses: " + numberOfWrongGuesses);
-            }
+
+            getGuessesOfPlayer(input,word, guesses);
+
+            // Checking to see if return value from method is true
+            // If true - game is over and player has won
             if(displayWordProgress(word, guesses)) {
-                System.out.println("You won! You got the word!");
+                System.out.println("Winner!");
                 break;
             }
         }
     }
 
-    public static boolean getGuessesOfPlayer(Scanner input, String word, List<Character> guesses) {
+    /*
+    Method for getting the guesses of the player.
+    Player is prompted to enter a letter to guess the word.
+    That guess is added to a list of guesses to keep track / add the guess to the word.
+    If the character guessed by the player is not in the word, it is flagged as an
+    incorrect guess and the player is prompted to make another attempt.
+     */
+    public static void getGuessesOfPlayer(Scanner input, String word, List<Character> guesses) {
         System.out.println("\nEnter a letter!");
         String letterGuessedByPlayer = input.next();
         guesses.add(letterGuessedByPlayer.charAt(0));
 
-        return word.contains(letterGuessedByPlayer);
+        if(!word.contains(letterGuessedByPlayer)){
+            System.out.println("Sorry! That guess wasn't right. Try again.\n");
+            System.out.println("Wrong guesses: " + numberOfWrongGuesses);
+            numberOfWrongGuesses += 1;
+        }
     }
 
+    /*
+    Boolean method for checking / displaying the state of the word being guessed.
+    If the return value evaluates to true, the game is over and the player has won the game.
+    Playing loop only ends if this condition is met / or if the limit of six incorrect guesses has been met.
+     */
     public static boolean displayWordProgress(String word, List<Character> guesses) {
         int numberOfCorrectGuesses  = 0;
         for(int i = 0; i < word.length(); i++) {
             if(guesses.contains(word.charAt(i))) {
                 System.out.print(word.charAt(i));
                 numberOfCorrectGuesses++;
-            } else {
+            }
+            else {
                 System.out.print(" - ");
-
             }
         }
         System.out.println();
-        return (word.length() == numberOfCorrectGuesses);
+        return word.length() == numberOfCorrectGuesses;
     }
 }
